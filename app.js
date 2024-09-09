3794b94b12a95da7d061e61ad6442f3bc2503ba5
zaid <jaid@giordano.com.sa>
2024-09-09 14:02:55 +0000

    new changes in the appjs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (request, response) => {
-  response.end('Hello, Jenkins with Docker!');
+  response.end('Hello, Jenkins with Docker! finaly I made it');
 };
 
 const server = http.createServer(requestHandler);
